f6db986b53612ff05ca864d4d73af3d563798876
Gerardo Hernandez <gahernandez02@gmail.com>
2019-11-14 21:22:25 -0800

    First commit with blank project file

---
+++ GrailDairy/GrailDairy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDairy
+//
+//  Created by Gerardo Hernandez on 11/14/19.
+//  Copyright © 2019 Gerardo Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
