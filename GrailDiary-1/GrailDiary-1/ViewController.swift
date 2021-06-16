cce338d92530a14577f2c7298a59c1e5c8f1dc41
Gerardo Hernandez <gahernandez02@gmail.com>
2021-06-15 18:54:55 -0700

    Added project file

---
+++ GrailDiary-1/GrailDiary-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GrailDiary-1
+//
+//  Created by Gerardo Hernandez on 6/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
