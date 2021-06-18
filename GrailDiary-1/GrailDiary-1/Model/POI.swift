8907d5daa4c5f0e2b0aad889f6d25f84a88b9a2f
Gerardo Hernandez <gahernandez02@gmail.com>
2021-06-18 16:51:20 -0700

    Added view controller files to match the view controllers in interface builder and a POI.swift file for the model.

---
+++ GrailDiary-1/GrailDiary-1/Model/POI.swift
@@ -0,0 +1,8 @@
+//
+//  POI.swift
+//  GrailDiary-1
+//
+//  Created by Gerardo Hernandez on 6/18/21.
+//
+
+import Foundation
---
+++ GrailDiary-1/GrailDiary-1/Views/POITableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  POITableViewCell.swift
+//  GrailDiary-1
+//
+//  Created by Gerardo Hernandez on 6/18/21.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
